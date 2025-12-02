a0d2a3b7d6a2750ad5cf6d84cb00be15b2faaa86
Nawahovvp <nawahovvp@gmail.com>
2025-12-02 17:45:31 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Partsgo-project-inventory/',  // ชื่อ repo ของคุณ – สำคัญมาก!
-  build: {
-    outDir: 'dist'  // ไฟล์ build จะไปที่ dist
-  }
+  base: '/Partsgo-project-inventory/',   // ต้องมีท้ายด้วย slash
 })
