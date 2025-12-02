f7af0c179dd37f87d39ebcaa8955236c60165ed3
Nawahovvp <nawahovvp@gmail.com>
2025-12-02 17:05:48 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+export default defineConfig({
+  plugins: [react()],
+  base: '/', // ถ้า deploy GitHub Pages เปลี่ยนเป็น '/partgo-react/'
+  build: {
+    outDir: 'dist'
+  }
+})
